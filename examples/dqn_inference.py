b8387b0d5eff54381e811f6ca8ed5debb41497f1
Luke Wood <lukewoodcs@gmail.com>
2020-08-15 16:50:46 -0700

    Implement frame buffering

--- examples/dqn_inference.py
+++ examples/dqn_inference.py
@@ -4,7 +4,8 @@ from sota_dqn import DQNInference
 
 env = gym.make("CartPole-v1")
 
-dqn = DQNInference(env=env, load_from="cartpole.model", warmup_actions=4)
+dqn = DQNInference(env=env, load_from="cartpole.model",
+                   frame_buffer_size=3, warmup_actions=4)
 
 for _ in range(10):
     dqn.play_round(sleep=1)
--- sota_dqn/dqn_inference.py
+++ sota_dqn/dqn_inference.py
@@ -3,6 +3,8 @@ import time
 
 from tensorflow.keras.models import load_model
 
+from collections import deque
+
 
 class DQNInference:
     def __init__(self,
@@ -10,6 +12,7 @@ class DQNInference:
                  model=None,
                  epsilon=0,
                  load_from=None,
+                 frame_buffer_size=1,
                  warmup_actions=1
                  ):
         if not env:
@@ -24,26 +27,35 @@ class DQNInference:
         else:
             self.model = model
 
+        self.frame_buffer = deque(maxlen=frame_buffer_size)
+        self.frame_buffer_size = frame_buffer_size
+
+    def buffer_to_input(self):
+        dims = (self.frame_buffer_size,) + self.env.observation_space.shape
+        result = np.zeros(dims)
+        for i, frame in enumerate(self.frame_buffer):
+            result[i] = frame
+        return np.expand_dims(result, axis=0)
+
     def pick_action(self, state):
         if np.random.random() < self.epsilon:
             return self.env.action_space.sample()
         return np.argmax(self.model.predict(state))
 
     def play_round(self, render=True, sleep=50):
-        input_dims = [
-            x if x is not None else 1 for x in self.model.input_shape]
-        state = self.env.reset().reshape(input_dims)
+        self.frame_buffer.append(self.env.reset())
         done = False
         reward = 0
         random_actions_taken = 0
         while not done:
+            state = self.buffer_to_input()
             if random_actions_taken < self.warmup_actions:
                 action = self.env.action_space.sample()
                 random_actions_taken = random_actions_taken + 1
             else:
                 action = self.pick_action(state)
-            state, next_reward, done, _ = self.env.step(action)
-            state = state.reshape(input_dims)
+            observation, next_reward, done, _ = self.env.step(action)
+            self.frame_buffer.append(observation)
             reward = reward + next_reward
             if render:
                 self.env.render()
