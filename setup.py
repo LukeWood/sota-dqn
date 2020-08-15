d310b6737d6afdf6d718e509b6d318d1d78d0e20
Luke Wood <lukewoodcs@gmail.com>
2020-08-15 12:54:12 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     packages=['sota_dqn'],
     version=VERSION,
     description='State of the art opinionanted DQN training and inference',
+    long_description=open("README.md", "r").read(),
+    long_description_content_type='text/markdown',
     include_package_data=True,
     url='https://github.com/lukewood/sota-dqn',
     author='Luke Wood',
